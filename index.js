d25aeeed4a5e53d217dc599a44b2b10e7d1c9a7d
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-19 14:52:06 +0300

    latest assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+
+var random =Math.ceil(Math.random() * 100);
+console.log(random);
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+let max = Math.max(1,4,8,12,16,20);
